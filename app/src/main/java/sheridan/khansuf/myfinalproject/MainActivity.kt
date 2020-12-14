21a9e8e1cd44fd9f9ce711114ec83665afc9a04d
sufwan <sufwan.khan@outlook.com>
2020-12-14 03:25:07 -0500

    Initial commit

---
+++ app/src/main/java/sheridan/khansuf/myfinalproject/MainActivity.kt
@@ -0,0 +1,18 @@
+package sheridan.khansuf.myfinalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import sheridan.khansuf.myfinalproject.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/sheridan/khansuf/myfinalproject/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package sheridan.khansuf.myfinalproject.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
